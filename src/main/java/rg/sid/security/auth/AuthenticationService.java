23d5f8cd53ca1838cfe7ebdf0e952417625fb859
landry1996 <ltchiengue73@gamail.com>
2023-08-02 01:38:49 +0100

    JWT_Authentication_With_Spring_Boot_3

---
+++ src/main/java/rg/sid/security/auth/AuthenticationService.java
@@ -0,0 +1,52 @@
+package rg.sid.security.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import rg.sid.security.config.JwtService;
+import rg.sid.security.user.Role;
+import rg.sid.security.user.User;
+import rg.sid.security.user.UserRepository;
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+    public AuthenticationResponse register(RegisterRequest request)
+    {
+        var user = User.builder()
+                .name(request.getName())
+                .surname(request.getSurname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        repository.save(user);
+
+           var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request)
+    {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = repository.findByEmail(request.getEmail())
+                .orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+}
---
+++ src/main/java/rg/sid/security/user/Role.java
@@ -0,0 +1,6 @@
+package rg.sid.security.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
